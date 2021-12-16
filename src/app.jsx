c08c7e0149d7dab261d69d75803771a871805ed6
ChuYeonBin <cndusqls98@gmail.com>
2021-12-16 17:14:24 +0900

    add maker_footer component

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './app.css';
+import MakerFooter from './components/maker_footer/maker_footer';
 import MakerHeader from './components/maker_header/maker_header';
 
 const App = () => {
   return (
     <>
       <MakerHeader />
+      <MakerFooter />
     </>
   );
 };
---
+++ src/components/maker_footer/maker_footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './maker_footer.module.css';
+
+const MakerFooter = () => {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.copyright}>Copyrightⓒ2021 by chuyeonbin</p>
+    </footer>
+  );
+};
+
+export default MakerFooter;
