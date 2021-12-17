6d7ebce77554c00ba04f1fb643b58b3ca96068e6
ChuYeonBin <cndusqls98@gmail.com>
2021-12-17 18:35:51 +0900

    add auth service & login component

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './app.css';
-import MakerFooter from './components/maker_footer/maker_footer';
-import MakerHeader from './components/maker_header/maker_header';
+import MakerLoginModal from './components/maker_login-modal/maker_login-modal';
 
 const App = () => {
   return (
     <>
-      <MakerHeader />
-      <MakerFooter />
+      <MakerLoginModal />
     </>
   );
 };
